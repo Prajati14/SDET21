0003de0d8c605c5f3306e707a02d3629705a3a40
prajati14 <prajati149@gmail.com>
2021-09-23 10:44:22 +0530

    hello

---
+++ Comcast_seleniumFramework/src/test/java/com/comcast/CreateOrganization/CreateOrganizationTest.java
@@ -0,0 +1,23 @@
+package com.comcast.CreateOrganization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+	
+	@Test
+	
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest");
+	}
+	
+	
+	@Test
+	public void createOrgWithContact() {
+		System.out.println("execute createOrgWithContact");
+	}
+	@Test
+	public void createOrgFromLead() {
+		System.out.println("execute createOrgFromLead");
+	}
+
+}
---
+++ Comcast_seleniumFramework/src/test/java/com/comcast/createContact/CreateContactTest.java
@@ -0,0 +1,21 @@
+package com.comcast.createContact;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void createContact() {
+		System.out.println("executing create contact");
+	}
+	@Test
+	public void createContactWithOrg() {
+		System.out.println("executing create Contact With Org ");
+	}
+	
+	@Test
+	public void deleteContact() {
+		System.out.println("execute delete contact");
+	}
+
+}
