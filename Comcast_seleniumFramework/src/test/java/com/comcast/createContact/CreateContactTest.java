c4f6e03eabd135bd7fd75c2171ef4fddb2f3bfbc
prajati14 <prajati149@gmail.com>
2021-09-23 14:47:07 +0530

    parameters

--- Comcast_seleniumFramework/src/test/java/com/comcast/createContact/CreateContactTest.java
+++ Comcast_seleniumFramework/src/test/java/com/comcast/createContact/CreateContactTest.java
@@ -4,9 +4,19 @@ import org.testng.annotations.Test;
 
 public class CreateContactTest {
 	
+	
+	
+	
+	
 	@Test(groups = "Smoke")
+	
 	public void createContact() {
 		System.out.println("executing create contact");
+		
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
 	}
 	@Test(groups = "Regression")
 	public void createContactWithOrg() {
